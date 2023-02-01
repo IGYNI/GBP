950b670911aab9e7dcf258f6c3e5d4162bcfe22b
Mr.Goodman36 <61795580+Goodman36@users.noreply.github.com>
2023-02-01 14:49:08 +0200

    [ADD] Can move items to inventory

---
+++ GBP/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public bool[] isFull;
+    public GameObject[] slots;
+    public List<string> _inventory;
+
+}
