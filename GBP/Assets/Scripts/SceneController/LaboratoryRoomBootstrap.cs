0d5107697f90d88af6daae3f8ed1cdef0f08d861
Serhii <greathazarius@gmail.com>
2023-02-01 17:39:43 +0200

    [ADD] Added laboratory room

---
+++ GBP/Assets/Scripts/SceneController/LaboratoryRoomBootstrap.cs
@@ -0,0 +1,15 @@
+using System;
+using Cysharp.Threading.Tasks;
+using SceneManagement;
+
+public class LaboratoryRoomBootstrap : RoomSceneController
+{
+	public override async UniTask Load(SceneContext sceneContext, IProgress<LoadingProgress> progress)
+	{
+		await UniTask.Yield();
+		OnLoad.Invoke();
+		SpawnPlayer(sceneContext);
+		//TODO initialize items;
+		progress.Report(new LoadingProgress() { progress = 1f });
+	}
+}
