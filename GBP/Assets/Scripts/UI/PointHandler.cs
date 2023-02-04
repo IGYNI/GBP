38c6b94bb845fda6656b29af9f13e71e5c57debf
IrePin <gagistos@gmail.com>
2023-02-04 19:04:57 +0200

    [MERGE] Cherry pick from FMOD

--- GBP/Assets/Scripts/UI/PointHandler.cs
+++ GBP/Assets/Scripts/UI/PointHandler.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PointHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class PointHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler 
 {
 
     public void OnPointerEnter(PointerEventData pointerEventData)
@@ -15,8 +15,4 @@ public class PointHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 
     }
 
-    public void OnPointerClick(PointerEventData pointerEventData)
-    {
-        //AudioManager.instance.InitializeMenuButtonClick();
-    }
 }
