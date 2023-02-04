00d66c229c962383db6a2b4b7970b90e8252b7a0
Serhii <greathazarius@gmail.com>
2023-02-04 17:02:28 +0200

    Continue

--- GBP/Assets/Scripts/UI/UIPlayerInventory.cs
+++ GBP/Assets/Scripts/UI/UIPlayerInventory.cs
@@ -6,15 +6,5 @@ using UnityEngine;
 [Obsolete("Deprecated",true)]
 public class UIPlayerInventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
