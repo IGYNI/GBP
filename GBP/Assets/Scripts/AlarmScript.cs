132a34105fffce8d2416f43b83952cc1fdfd607b
IrePin <gagistos@gmail.com>
2023-02-04 22:52:36 +0200

    [ADD] Added logic to kitchen room

--- GBP/Assets/Scripts/AlarmScript.cs
+++ GBP/Assets/Scripts/AlarmScript.cs
@@ -1,48 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AlarmScript : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    [SerializeField] Light _lightR;
-    [SerializeField] Light _lightL;
-    [SerializeField] bool _alarm;
+	[SerializeField] private float speed;
+	[SerializeField] private Light _lightR;
+	[SerializeField] private Light _lightL;
+	[SerializeField] private bool _alarm;
+	[SerializeField] private bool playOnStart;
 
-    public bool _Alarm
-    {
-        get { return _alarm; }
-        set { _alarm = value; }
-    }
-    void Start()
-    {
 
-    }
+	public bool Alarm
+	{
+		get => _alarm;
+		set => _alarm = value;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (_alarm)
-        {
-            Play();
-        }
-        else if (!_alarm)
-        {
-            Stop();
-        }
-    }
-    public void Play()
-    {
-        _lightR.enabled = true;
-        _lightL.enabled = true;
-        _lightL.transform.Rotate(0, speed * Time.deltaTime, 0);
-        _lightR.transform.Rotate(0, speed * Time.deltaTime, 0);
-    }
-    public void Stop()
-    {
-        _lightR.enabled = false;
-        _lightL.enabled = false;
-        _lightR.transform.Rotate(0, 0, 0);
-        _lightL.transform.Rotate(0, 0, 0);
-    }
+	private void Start()
+	{
+		if (playOnStart)
+			Alarm = true;
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		switch (_alarm)
+		{
+			case true:
+				Play();
+				break;
+			case false:
+				Stop();
+				break;
+		}
+	}
+
+	public void Play()
+	{
+		_lightR.enabled = true;
+		_lightL.enabled = true;
+		_lightL.transform.Rotate(0, speed * Time.deltaTime, 0);
+		_lightR.transform.Rotate(0, speed * Time.deltaTime, 0);
+	}
+
+	public void Stop()
+	{
+		_lightR.enabled = false;
+		_lightL.enabled = false;
+		_lightR.transform.Rotate(0, 0, 0);
+		_lightL.transform.Rotate(0, 0, 0);
+	}
 }
