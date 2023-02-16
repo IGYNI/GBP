c7b6c7d43cdf9392b6ca7b3c4e43b07f6a04a071
Mr.Goodman36 <61795580+Goodman36@users.noreply.github.com>
2023-02-16 16:39:33 +0200

    [ADD] RotationPuzzlePrototype

---
+++ GBP/Assets/Scripts/RotationPuzzle/RotationPuzzle.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RotationPuzzle : Puzzle
+{
+    [SerializeField] private Camera raycastCamera;
+    [SerializeField] private LayerMask _layerMask;
+    public List<RotatePipe> pipes;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            UpdateRaycast();
+        }
+        CheckLogic();
+    }
+    private void UpdateRaycast()
+    {
+        //if (_blockInput)
+        // return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray mRay = raycastCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(mRay.origin, mRay.direction, out RaycastHit hitInfo, 100))
+            {
+                var pipe = hitInfo.collider.gameObject.GetComponentInParent<RotatePipe>();
+                if (pipe != null)
+                {
+                    
+                    pipe.Rotate();                
+                }
+            }
+        }
+    }
+    private void CheckLogic()
+    {
+        bool allMatch = true;
+        for (int i = 0; i < pipes.Count; i++)
+        {
+            if (pipes[i].Active)
+            {
+                if (pipes[i].IsMatch() == false)
+                {
+                    allMatch = false;
+                    break;
+                }
+            }
+        }
+        if (allMatch)
+        {
+            Debug.Log("PuzzleComplete");
+        }
+    }
+
+}
+
